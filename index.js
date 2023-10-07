5db557711ecd362e38d24932152a518d8bb7bf5e
Ajay Kumar <ajayit2020@gmail.com>
2023-10-07 21:32:03 +0530

    added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3001
-
+'use strict';
+ 
+const express = require('express');
+ 
+// Constants
+const PORT = 3001;
+const HOST = '0.0.0.0';
+ 
+// App
+const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  res.send('Hello World');
+});
+ 
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
