0ade6485001d9d24eb5552470150fb4d5eb668fc
Ajay Kumar <30952347+imajkumar@users.noreply.github.com>
2023-10-13 00:43:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World AP CICD.... complete dev');
+  res.send('Backed API TEST Success');
 });
  
 app.listen(PORT, HOST, () => {
