e3701f74f9fade50456bcc120485b814f2456173
Ajay Kumar <ajayit2020@gmail.com>
2023-10-13 19:32:21 +0530

    added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World...dev ');
+  res.send('Hello World...dev777 ');
 });
  
 app.listen(PORT, HOST, () => {
